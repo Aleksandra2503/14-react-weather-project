e186bfd1c595a897d4fff04e2c66db68f561d164
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-14 21:26:15 +0100

    Commit Weather js and WeatherForecast js

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import axios from "axios";
 import WeatherInfo from './WeatherInfo';
-import WeatherForecast from './WeatherForecast';
+// import WeatherForecast from './WeatherForecast';
 import "./Weather.css";
 
 
@@ -63,7 +63,7 @@ export default function Weather(props) {
                     </div>
                 </form>
                 <WeatherInfo data={weatherData} />
-                <WeatherForecast coordinates={weatherData.coordinates} />
+                {/* <WeatherForecast coordinates={weatherData.coordinates} /> */}
                 </div>
         );
     } else {
